7d6f98fb1eba391af7e6c3ec181e9a51165c5394
ArtMed <artiebears@mail.ru>
2023-10-14 00:33:24 +0300

    create gui

--- documents_parser/ui/main.py
+++ documents_parser/ui/main.py
@@ -1,25 +1,68 @@
-import streamlit as st
+import time
 
-def run_file_processing(filename):
-    print(filename)
+import streamlit as st
+from pathlib import Path
+from PIL import Image
+
+SRC_PATH = Path(__file__).parent / "src"
+DOWNLOAD_FILENAME = Path("documents_parser/incoming_files/file.pdf")
+
+def test():
+    time.sleep(5)
+
+class Gui:
+    def __init__(self):
+        st.set_page_config(layout='wide')
+        self.uploaded_file = None
+        # self.button_status = ButtonStatus(disabled=True)
+        self.button_disabled = True
+        self.check_button: st.button or None = None
+        self.draw_gui()
+
+    def draw_gui(self):
+        # title, logo = st.columns(2)
+        title, logo = st.columns([5, 1])
+        title.title("File Uploader by \"AAA_Team\"")
+        logo.image(str(SRC_PATH / "logo.png"), width=150, output_format="PNG")
+        self.draw_choose_file()
+        # st.image(str(SRC_PATH / "dance.gif"), output_format='GIF')
+        self.check_button = st.button(
+            'Check',
+            help="Check document",
+            on_click=lambda: self.run_file_processing(),
+            disabled=self.button_disabled
+        )
+
+    def draw_choose_file(self):
+        self.uploaded_file = st.file_uploader(
+            "Choose a file",
+            type=["pdf"],
+            accept_multiple_files=False
+        )
+        if self.uploaded_file is not None:
+            filename = self.uploaded_file.name
+            # uploaded_file.write('')
+            st.write("Selected file:", filename)
+            self.button_disabled = False
+
+        else:
+            st.write("No file selected")
+
+    def run_file_processing(self):
+        self.button_disabled = True
+        with open(DOWNLOAD_FILENAME, 'wb+') as f:
+            f.write(self.uploaded_file.read())
+        print(self.uploaded_file.name)
+        gif_path = "https://donskow.com/g"
+
+        gif_runner = st.image(gif_path)
+        test()
+        gif_runner.empty()
+        self.uploaded_file = None
 
 
 def main():
-    filename: str | None = None
-    st.title("File Uploader")
-    check_button = st.button(
-        'Check',
-        help="Check document",
-        on_click=lambda: run_file_processing(filename)
-    )
-    uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
-
-    if uploaded_file is not None:
-        filename = uploaded_file.name
-        st.write("Selected file:", filename)
-
-    else:
-        st.write("No file selected")
+    ui = Gui()
 
 
 if __name__ == '__main__':
---
+++ documents_parser/ui/src/gif_creator.py
@@ -0,0 +1,40 @@
+from PIL import Image
+
+image_path = 'img.png'
+ico_path = 'icon.ico'
+output_path = 'output.png'
+
+def place_ico_on_image(image_path, ico_path, step, i):
+    # Open the image and ICO files
+    image = Image.open(image_path)
+    ico = Image.open(ico_path)
+
+    # Resize the ICO to a desired size (optional)
+    # ico = ico.resize((50, 50))
+
+    # Calculate the position to place the ICO on the image
+    position = (0 + step, 0)
+    print(step)
+    # Paste the ICO onto the image at the calculated position
+    image.paste(ico, position, mask=ico)
+
+    # Save the modified image with the ICO placed on it
+    if 10 > i > 0:
+        image.save(f"gif/output-00{i}.png")
+    elif i<100:
+        image.save(f"gif/output-0{i}.png")
+    else:
+        image.save(f"gif/output-{i}.png")
+
+def spam():
+    step = 0
+    i = 0
+    ds = 10
+    while step < 1472:
+        place_ico_on_image(image_path, ico_path, step=step, i=i)
+        step += ds
+        i += 1
+# Example usage:
+
+
+spam()
