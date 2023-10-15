cb9fceb4d0eab1f876ee324daeaedb508932a2ee
Andrey Donskoy <donskoi.com@gmail.com>
2023-10-15 10:25:09 +0300

    add docs

--- documents_parser/utils/extraction.py
+++ documents_parser/utils/extraction.py
@@ -3,6 +3,13 @@ import numpy as np
 
 
 def extract_text(img: np.ndarray) -> str:
+    """
+    Extract text from the image using Tesseract
+
+    :param img: image
+    :return:
+        Extracted text
+    """
     text = pytesseract.image_to_string(img, lang='rus')
     text = text.strip().replace("\n", " ").strip()
     return text
